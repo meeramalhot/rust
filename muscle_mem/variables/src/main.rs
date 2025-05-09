33ee02e3e316ccc3f5849c08f296e9eaf4310687
meera <meera.malhot@gmail.com>
2025-05-09 16:56:50 -0400

    variables

--- muscle_mem/variables/src/main.rs
+++ muscle_mem/variables/src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
+    println!("The value of x is: {x}");
+
+    //cant mut a constant
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("{}", THREE_HOURS_IN_SECONDS);
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
     println!("The value of x is: {x}");
 }
